29a63e0ab6fa7a678adf5c6661e491a17a657468
Anthony Goddard <anthony@anthonygoddard.com>
2015-01-05 23:24:38 -0700

    WIP - add initial teapot metadata and decision detail

--- lib/jackal-kitchen-judge/adjudicate.rb
+++ lib/jackal-kitchen-judge/adjudicate.rb
@@ -1,4 +1,4 @@
-require 'jackal-kitchen-judge'
+require "jackal-kitchen-judge"
 
 module Jackal
   # Formulate kitchen output into short message
@@ -14,14 +14,16 @@ module Jackal
       # @return [Truthy, Falsey]
       def valid?(message)
         super do |payload|
-          payload.get(:data, :kitchen)
+          payload.get(:data, :kitchen, :result)
         end
       end
 
       def execute(msg)
         failure_wrap(msg) do |payload|
           payload[:data][:kitchen][:judge] = Smash.new
-          payload[:data][:kitchen][:judge][:judgement] = adjudicate(payload)
+          verdict = adjudicate(payload)
+          payload[:data][:kitchen][:judge][:decision] = verdict[:decision]
+          payload[:data][:kitchen][:judge][:reasons] = verdict[:reasons]
           job_completed(:kitchen_judge, payload, msg)
         end
       end
@@ -29,13 +31,60 @@ module Jackal
       # payload:: Payload
       # Return true if tests passed
       def adjudicate(payload)
-        rspec_result = payload.get(:data, :kitchen, :result, 'bundle exec rspec')
-        kitchen_result = payload.get(:data, :kitchen, :result, 'bundle exec kitchen test')
-        if rspec_result == :success && kitchen_result == :success
-          return true
-        else
-          return false
-        end
+        teapot_data = teapot_metadata(payload.get(:data, :kitchen, :test_output, :teapot))
+        payload[:data][:kitchen][:judge][:teapot] = teapot_data
+
+        judgement = { :reasons => [] }
+
+        judgement[:reasons] << :teapot_runtime if teapot_data[:total_runtime][:threshold_exceeded]
+
+        rspec_result = payload.get(:data, :kitchen, :result, "bundle exec rspec")
+        kitchen_result = payload.get(:data, :kitchen, :result, "bundle exec kitchen test")
+
+        judgement[:reasons] << :rspec unless rspec_result == :success
+        judgement[:reasons] << :kitchen unless kitchen_result == :success
+
+        judgement[:decision] = judgement[:reasons].empty?
+
+        judgement
+      end
+
+      def teapot_metadata(data) # data,kitchen,test_output,teapot ->  #return hash
+        # TODO :transient_failures => [],
+
+        duration = data["timing"].map { |d|d["time"] }.inject(:+)
+
+        exceeded = duration > Carnivore::Config.get(
+                     :kitchen, :thresholds, :teapot, :total_runtime
+                   )
+
+        total_runtime = { :duration => duration,
+                          :threshold_exceeded => exceeded }
+
+        sorted_resources = data["timing"].sort_by{ |x|x["time"] }
+        slowest_resource = sorted_resources.last
+
+        resources_over_threshold = sorted_resources.reject { |r|
+          r["time"] < Carnivore::Config.get(
+            :kitchen, :thresholds, :teapot, :resource_runtime
+          )
+        }
+
+        { :slowest_resource => slowest_resource,
+          :resources_over_threshold => resources_over_threshold,
+          :total_runtime => total_runtime }
+      end
+
+      def serverspec_metadata(data)
+        #total serverspec run time
+        #serverspec tests over threshold
+        #longest serverspec test
+      end
+
+      def chefspec_metadata(data)
+        #total chefspec run time
+        #longest chefspec test
+        #chefspec tests over threshold
       end
 
     end
